c4956405f5b8a9ca6ac498dbb9f9a35a734690ec
harrimeskanen <meskanen.harri@gmail.com>
2020-03-07 13:06:37 +0200

    refactor: DateType attribute 'key' now np array.

--- Database.py
+++ Database.py
@@ -4,12 +4,13 @@ from Target import Target
 from Date import DateType, Year, Month, Day
 import Utility 
 from datetime import datetime
+import numpy as np
 
 
-class Database(DateType): 
-    x = 10000
+class Database(DateType):
+    C = np.array( (1, 0, 0) )
     def __init__( self, data ):
-        super(Database, self).__init__(0)
+        super(Database, self).__init__(np.array( (0, 0, 0) ))
         self.number_of_transactions  = 0
         self.number_of_targets  = 0
         self.key_max_length     = 0
@@ -36,34 +37,10 @@ class Database(DateType):
         Number of transaction transactions created: " + str(self.number_of_transactions) + "\n\
         Number of transaction targets created: " + str(self.number_of_targets))
 
-    def child_factory(self, key, n):
-        return Year(key, n) 
+
+    def child_factory(self, key):
+        return Year(key) 
 
     def datetime_to_key(self, date):
-        return self.x * date.year + Year.x * date.month + Month.x * date.day
-
-    def get_all(self):
-        return self.children
-
-    def get_year(self, year_number):
-        key = self.generate_key(year_number)
-        if year_number not in self:
-            return Year(key, year_number)
-        return self.children[year_number]
-
-    def get_month(self, year, month):
-        year = self.get_year(year)        
-        return year.get_child(month)
-
-    def get_day(self, year, month, day):
-        month = self.get_month(year, month)
-        return month.get_child(day)
-
-    def add_transaction( self, transaction, key):
-        c = self.get_child(key)
-        c.add_transaction(transaction, key)
-        self.children[c.key] = c
-        self.transactions.append(transaction)
-        self.number_of_transactions += 1
-
+        return np.array( (date.year, date.month, date.day) )
 
--- Date.py
+++ Date.py
@@ -2,106 +2,114 @@ import numpy as np
 
 
 class DateType:
-    x = 0
+    # self mask
+    S = np.zeros(3)
+    # child mask
+    C = np.zeros(3)
     def __init__(self, key=None, n=None):
-        # unique id e.g. 20201120
+        # e.g. np.array( [1992, 6, 23] )
         self.key  = key
-        self.key_str = self.__key_str__()
-        # self is parent's nth children
-        self.n = n
+        self.key_str = self.key_to_str(key)
         # Map<date_key, DateType>, sub datetypes e.g. this==Month --> children are Days
         self.children   = {}
         # List<Transaction>, transactions created during datetype(this) period
         self.transactions    = []
 
+
+    def child_factory(self, key):
+        return 0
+
+
     def __str__(self):
         return self.key_str
 
-    def __key_str__(self):
-        key_str = str(self.key)
-        if len(key_str) != 8:
-            return key_str
-        key_lst = np.array([key_str[6:8], key_str[4:6], key_str[0:4]]).astype(int)
-        key_lst = key_lst[key_lst > 0].astype(str)
-        return ".".join(key_lst)
 
-    # VIRTUAL
-    def child_factory(self, key, n):
-        return 0
+    def key_to_str(self, key):
+        key_nonzero = key[key > 0]
+        return ".".join(key_nonzero.astype(int).astype(str))
     
-    # generate key for nth child
-    def generate_key(self, n):
-        return self.key + n * self.x
 
-    def mask_key(self, key):
-        n = self.decode_key(key)
-        return self.generate_key(n)
+    # generate key for nth child EITHER from int or key
+    def get_child_key(self, n_or_key):
+        return self.key + self.C * n_or_key
 
-    # key --> my nth children
-    def decode_key(self, key):
-        return int( (key - self.key) / self.x )
 
-    # generate key (from key) for nth child
-    def encode_key(self, key):
-        return self.key + self.decode_key(key) * self.x
+    def __is_self(self, key):
+        return (key == self.key).all() == True
 
-    def __key_is_self(self, key):
-        return key == self.key
 
-    def __key_is_descendant(self, key):
-        return (self.key % key) == self.key
+    # is excisting child !!!
+    def __is_child(self, key):
+        return self.key_to_str(key) in self.children 
 
-    def __add_new_child(self, key):
-        n = self.decode_key(key)
-        child = self.child_factory(key, n)
-        self.children[key] = child
 
     def get_child(self, key):
-        child_key = self.mask_key(key)
-        if child_key not in self.children:
-            self.__add_new_child(child_key)
-        return self.children[child_key]
+        # check if key is me
+        if self.__is_self(key):
+            return self
+        
+        # if not me then create key for child
+        child_key = self.get_child_key(key)
+        child_key_str = self.key_to_str(child_key)
+
+        # create new if child doesnt exist yet
+        if not self.__is_child(child_key):
+            self.children[child_key_str] = self.child_factory(child_key)
+
+        # iterate recursively
+        return self.children[child_key_str].get_child(key)
+
 
     def add_transaction(self, transaction, key):
-        if self.__key_is_self(key):
-            self.transactions.append(transaction)
+        # add transaction to parent's transactions too... 
+        self.transactions.append(transaction)
+
+        # break iteration if key is me
+        if self.__is_self(key):            
             return
         
-        elif self.__key_is_descendant(key):
-            c = self.get_child(key)
-            c.add_transaction(transaction, key)
-            self.children[c.key] = c
-            self.transactions.append(transaction)
+        # if key is no me then it is one my children
+        child_key = self.get_child_key(key)
+        child_key_str = self.key_to_str(child_key)
 
-        else:
-            return # shouldn't go here, ever
+        # create new if child doesnt exist yet
+        if not self.__is_child(child_key):
+            self.children[child_key_str] = self.child_factory(child_key)
+
+        # iterate recursively
+        self.children[child_key_str].add_transaction(transaction, key)
         
 
 
 
 class Year(DateType):
-    x = 100    
-    def __init__(self, key, n):
-        super(Year, self).__init__(key, n)  
-      
-    def child_factory(self, key, n):
-        return Month(key, n) 
+    S = np.array(( 1, 0, 0 ))
+    C = np.array(( 0, 1, 0 ))
+    def __init__(self, key):
+        super(Year, self).__init__(key)  
+
+
+    def child_factory(self, key):
+        return Month(key) 
 
 
 
 class Month(DateType):
-    x = 1 
-    def __init__(self, key, n):
-        super(Month, self).__init__(key, n)
+    S = np.array(( 1, 1, 0 ))
+    C = np.array(( 0, 0, 1 ))
+    def __init__(self, key):
+        super(Month, self).__init__(key)
 
-    def child_factory(self, key, n):
-        return Day(key, n)
+
+    def child_factory(self, key):
+        return Day(key)
 
 
 
 class Day(DateType):
-    def __init__(self, key, n):
-        super(Day, self).__init__(key, n)
+    S = np.array(( 1, 1, 1 ))
+    def __init__(self, key):
+        super(Day, self).__init__(key)
         
 
 
