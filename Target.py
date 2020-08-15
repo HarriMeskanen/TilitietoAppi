afbadf14f4e857d89cae21d5018a653c49264ebf
unknown <harri.meskanen@student.tut.fi>
2020-08-15 15:33:55 +0300

    Defined new datatype 'TargetContainer'. Datatype 'Target' now inherits 'TransactionContainer' type

--- Target.py
+++ Target.py
@@ -1,25 +1,63 @@
+import Transaction
 
 
-class Target:
-    def __init__( self, name ):
-        self.name = name
-        self.transactions = []
-        self.sum = 0
+class TargetContainer:
+    def __init__(self):
+        self.targets = {}
+        self.key_max_length = 0
 
-    def add_transaction(self, transaction):
-        self.transactions.append(transaction)
-        self.sum += transaction.val
 
     def __contains__(self, key):
-        return key == self.name
+        return key in self.targets
 
-    def __str__(self):
-        s = self.name + "\n"
-        for t in self.transactions:
-                s = s + "\t" + t.date.strftime('%d.%m.%Y') + "\t" + str(format(t.val, ".2f")) + "\n"
-        s = s + "YHT. \t" + str( format(self.sum, ".2f"))
-        return s
 
-    def __len__(self): 
+    def add_target(self, t):
+        self.targets[t.name] = t
+
+        if len(t) > self.key_max_length:
+            self.key_max_length = len(t)
+
+
+    def add_transaction(self, t):
+        if t.target not in self:            
+            self.add_target(Target(t.target) )
+
+        self.targets[t.target].add_transaction(t)
+
+
+    def get_target_log(self):
+        log = ''
+
+        for t in self.targets.values():
+            log += '\n\n\n' + t.get_transaction_log()
+
+        return log
+
+
+
+
+class Target(Transaction.TransactionContainer):
+    def __init__( self, name ):
+        Transaction.TransactionContainer.__init__(self)
+        self.name = name
+        # e.g. 'food' or 'car' or 'hobby'
+        self.category = str
+
+    def __len__(self):
         return len(self.name)
+
+
+    def get_transaction_log(self):
+        log = ''
+
+        log += self.name + '\n' + '-' * len(self)
+
+        for t in self.transactions:
+            log += '\n' + t.get_date_str() + '\t' + t.get_val_str()
+
+        log += '\nINCOMES  \t' + str(format(self.incomes,  '.2f'))
+        log += '\nEXPENSES \t' + str(format(self.expenses, '.2f'))
+        log += '\nNETSUM   \t' + str(format(self.netsum,   '.2f'))
+
+        return log
 
