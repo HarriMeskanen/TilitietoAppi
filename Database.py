8a7788295fe1143dd612fea9013741a1d1948192
unknown <harri.meskanen@student.tut.fi>
2020-08-15 15:43:36 +0300

    File update

--- Database.py
+++ Database.py
@@ -9,45 +9,51 @@ import numpy as np
 
 class Database(DateType):
     C = np.array( (1, 0, 0) )
+
     def __init__( self, data ):
         super(Database, self).__init__(np.array( (0, 0, 0) ))
         self.key_str = "output"
-        self.number_of_transactions  = 0
-        self.number_of_datetypes    = 0
-        self.number_of_targets  = 0
+        self.t0 = None
+        self.tk = None
     
         for data_row in data:
             try:
                 # parse date
-                date, val, target =  Utility.get_transaction_data(data_row)
+                date, val, target_name =  Utility.get_transaction_data(data_row)
+                
+                # see if this is youngest of oldest transaction instance
+                self.update_date_limits(date)
+                
                 # create new transaction
-                transaction = Transaction(date, val, target)
+                transaction = Transaction(date, val, target_name)
+
                 # add transaction to database
                 transaction_key = self.datetime_to_key(transaction.date)
-                self.add_transaction( transaction, transaction_key ) 
-                self.number_of_transactions += 1
-                print(Utility.bcolors.ENDC + "[Database]{e}".format(e=transaction))
+                self.add_transaction( transaction, transaction_key )
+                print(Utility.bcolors.ENDC + "New Transaction {t}".format(t=transaction))
+
 
             except Utility.InvalidDataException as e_data:
                 print(Utility.bcolors.WARNING + e_data.message)
-                print(Utility.bcolors.FAIL + "[Database]{dr}".format(dr=data_row))
+                print(Utility.bcolors.FAIL + "{dr}".format(dr=data_row))
                 continue
 
             except Exception as e:
                 print( Utility.bcolors.FAIL + str(e.args) )
 
-        self.iterate(self)
 
     def child_factory(self, key):
         return Year(key) 
 
+
     def datetime_to_key(self, date):
         return np.array( (date.year, date.month, date.day) )
 
-    def iterate(self, dt):
 
-        for child in dt.children.values():
-            self.number_of_datetypes += 1
-            self.iterate(child)
+    def update_date_limits(self, date):
+        if self.t0 == None or date < self.t0:
+            self.t0 = date
 
+        if self.tk == None or date > self.tk:
+            self.tk = date
 
--- main.py
+++ main.py
@@ -1,7 +1,6 @@
 import sys
 import time
 from Database import Database
-from DataProcessor import DataProcessor
 from ipop import IO
 import Utility as util
 
@@ -20,23 +19,14 @@ def main(filename=None):
     data = io.get_data( filename )
     db   = Database( data )    
     print(util.bcolors.OKGREEN + "...\n[COMPLETE] Database build finished\
-    \nNumber of transactions saved: " + str(db.number_of_transactions) +
-    "\nNumber of DateType objects created: " + str(db.number_of_datetypes))
-    time.sleep(1)
-    print(util.bcolors.OKGREEN + "...\nStarting to process collected data...")
-    time.sleep(1)
-
-    # process data
-    prcsr  = DataProcessor()
-    output = prcsr.process(db)
-    print(util.bcolors.OKGREEN + "...\nData processing completed...")
-    time.sleep(1)
+    \nNumber of transactions saved: " + str(len(db.transactions)) +
+    "\nTime period: " + str(db.t0) + " - " + str(db.tk))
 
 
     # save and present results
     print(util.bcolors.OKGREEN + "...\nSaving results to " + io.root)
     time.sleep(1)
-    io.save_output(output, io.root + '\\' + output.key)
+    io.save_data(db, db.key_str + "\\")
 
     print(util.bcolors.OKGREEN + "...\nProgram finished successfully. Exiting...")
 
